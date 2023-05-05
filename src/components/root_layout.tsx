3b570c432ee39437432f7452392c671df062e6e3
jackcruz53 <jacklivingston53@gmail.com>
2023-05-05 10:58:19 -0700

    -attempt 1

--- src/components/root_layout.tsx
+++ src/components/root_layout.tsx
@@ -2,7 +2,8 @@ import {FC} from 'react'
 
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image'
-import NavBar from 'react-bootstrap/NavBar'
+import Navbar from 'react-bootstrap/Navbar'
+import NavbarBrand from 'react-bootstrap/NavbarBrand'
 
 interface RootLayoutProps {
   children: React.ReactNode,
@@ -20,11 +21,11 @@ const RootLayout: FC<RootLayoutProps> = ({children}) => {
   return (
     <div style={{marginLeft: "auto", marginRight: "auto", maxWidth: 800, backgroundColor: "rgba(231, 231, 231)"}}>
       {/* TODO: Add a menu */}
-      <NavBar style={{backgroundColor: "green"}}>
+      <Navbar style={{backgroundColor: "green"}}>
         <Container>
-          <NavBar.Brand><span style={{color: "gold", fontSize: "2rem"}}>BASC QA Tool</span></NavBar.Brand>
+          <NavbarBrand><span style={{color: "gold", fontSize: "2rem"}}>BASC QA Tool</span></NavbarBrand>
         </Container>
-      </NavBar>
+      </Navbar>
       <div style={{paddingTop: "1rem"}}>
         {children}
       </div>
