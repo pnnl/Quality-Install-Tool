90b1a4bfe3687d60199d9503d704aec568b71db8
Rae Chen <cxy7562@gmail.com>
2023-05-23 13:57:53 -0700

    doc & fix following comment

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -1,14 +1,30 @@
 import React, { FC } from 'react';
 
-interface MyDateProps {
-    dateparam: string,
+/**
+ * Interface for the DateStr prop
+ */
+interface DateStr {
+    date: string,
     locals: string,
     options: any
-  }
+}
 
-const MyDate: FC<MyDateProps>= ({dateparam, locals = 'en-us', options = {timeZone: 'UTC', month:"long", day:"numeric", year:"numeric"}}) => {
-  const date = new Date(dateparam);
-  const formattedDate = date.toLocaleDateString(locals, options);
+/**
+ * Default options for date formatting
+ */
+const DEFAULT_OPTIONS = {timeZone: 'UTC', month:"long", day:"numeric", year:"numeric"};
+
+/**
+ * MyDate component
+ * @param date - The date string
+ * @param locals - The locals string for locale-specific formatting (default: 'en-us')
+ * @param options - Additional options for date formatting (optional)
+ * @returns The formatted date component
+ */
+const MyDate: FC<DateStr>= ({date, locals = 'en-us', options = {}}) => {
+  const full_options = {...DEFAULT_OPTIONS, ...options};
+  const parsedDate = new Date(date);
+  const formattedDate = parsedDate.toLocaleDateString(locals, full_options);
 
   return <span>{formattedDate}</span>;
 }
