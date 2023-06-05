06c70bfaccd121767f0480e2ddebbfaf0923877d
Michael Georgescu <michael.georgescu@pnnl.gov>
2023-06-05 13:53:02 -0700

    replace a href with react router link

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,14 +1,16 @@
 import {Component, FC} from 'react'
 import templatesConfig from '../templates/templates_config'
 import { ListGroup } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
 
 const Home: FC = () => {
     
   const templates = Object.keys(templatesConfig).map((key) => (
     <ListGroup.Item key={key}>
-      <a href={`/app/${key}`}>
+      <Link to={`/app/${key}/job1`}>
       {templatesConfig[key as keyof typeof templatesConfig].title}
-      </a>
+      </Link>
     </ListGroup.Item>
   ));
     
