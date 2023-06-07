13eda05a0003c7b226601e9e4fa3ebff76c3f52d
Rae Chen <cxy7562@gmail.com>
2023-06-07 14:03:45 -0700

    doc change

--- src/utilities/paths_utils.tsx
+++ src/utilities/paths_utils.tsx
@@ -4,10 +4,10 @@ import { isUndefined, toPath } from 'lodash'
  * Utility function used to convert a string path into a string 
  * suitable as an html id value
  * 
- * @param prefix A string used as a first path component
  * @param path A string representing a path into a JSON-like object
  * (e.g. ""location[1].state"")
- * @param separator String used to separate the path components (default: "-")
+ * @param prefix (OPTIONAL) A string used as a first path component (default: "path")
+ * @param separator (OPTIONAL) String used to separate the path components (default: "-")
  * @returns 
  */
 export function pathToId(path: string, prefix?: string, separator?:string): string {
