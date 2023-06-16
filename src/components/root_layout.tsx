7ff30226b4567017c5dfc76759b663cae1c91a35
Rae Chen <cxy7562@gmail.com>
2023-06-16 14:04:56 -0700

    back function

--- src/components/root_layout.tsx
+++ src/components/root_layout.tsx
@@ -1,8 +1,11 @@
 import {FC} from 'react'
+import { Button } from 'react-bootstrap';
+import { TfiAngleLeft } from "react-icons/tfi";
 
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image'
 import NavBar from 'react-bootstrap/NavBar'
+import { useLocation, Link } from "react-router-dom";
 
 interface RootLayoutProps {
   children: React.ReactNode,
@@ -17,12 +20,46 @@ interface RootLayoutProps {
  * Provides a banner that includes a menu
  */
 const RootLayout: FC<RootLayoutProps> = ({children}) => {
+
+  const location = useLocation();
+
+  // Regular expression pattern to match the desired paths
+  const regexPatternToHome = /^(.*)\/app\/([^\/]+)$/;
+  const regexPatternToHPWH = /^(.*)\/app\/qa_hpwh\/([^\/]+)$/
+  const regexPatternToPlayGround = /^(.*)\/app\/playground\/([^\/]+)$/
+
+  // Determine whether to show the back button based on the current route
+  const showBackButtonToHome = regexPatternToHome.test(location.pathname);
+  const showBackButtonToHPWH = regexPatternToHPWH.test(location.pathname);
+  const showBackButtonToPlayGround = regexPatternToPlayGround.test(location.pathname);
+  
   return (
     <div style={{marginLeft: "auto", marginRight: "auto", maxWidth: 800, backgroundColor: "rgba(231, 231, 231)"}}>
       {/* TODO: Add a menu */}
       <NavBar style={{backgroundColor: "green"}}>
-        <Container>
-          <NavBar.Brand><span style={{color: "gold", fontSize: "2rem"}}>Quality Install Tool</span></NavBar.Brand>
+        {showBackButtonToHome && (
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <TfiAngleLeft  style={{ marginLeft: "1rem", marginRight: "1rem", color: "white" }}/>
+          </Link>
+        )}
+        {showBackButtonToHPWH && (
+          <Link to="/app/qa_hpwh" style={{ textDecoration: "none" }}>
+            <TfiAngleLeft  style={{ marginLeft: "1rem", marginRight: "1rem", color: "white" }}/>
+          </Link>
+        )}
+        {showBackButtonToPlayGround && (
+          <Link to="/app/playground" style={{ textDecoration: "none" }}>
+            <TfiAngleLeft  style={{ marginLeft: "1rem", marginRight: "1rem", color: "white" }}/>
+          </Link>
+        )}
+        <Container
+        style = {{display: "flex",
+        alignItems: "center",
+        justifyContent: "center",}}>
+          <NavBar.Brand><span style={{
+            color: "gold", 
+            fontSize: "2rem", 
+          }}>Quality Install Tool</span></NavBar.Brand>
         </Container>
       </NavBar>
       <div style={{paddingTop: "1rem"}}>
