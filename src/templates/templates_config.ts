5a2873dcb1694ed85e14b14d724a3426880561b7
holl695 <charlie.holly@pnnl.com>
2023-07-02 11:16:13 -0700

    Copied original qa_hpwh.mdx to old-qa_hpwh.mdx

--- src/templates/templates_config.ts
+++ src/templates/templates_config.ts
@@ -1,4 +1,5 @@
 import QaHPWHTemplate from './qa_hpwh.mdx'
+import OldQaHPWHTemplate from './old-qa_hpwh.mdx'
 import MdxPlayground from './playground.mdx'
 import { MDXProps } from 'mdx/types';
 
@@ -13,6 +14,10 @@ const templatesConfig : TemplateConfig = {
   qa_hpwh: {
     title: "Heat Pump Water Heater",
     template: QaHPWHTemplate,
+  },
+  old_qa_hpwh: {
+    title: "Old – Heat Pump Water Heater",
+    template: OldQaHPWHTemplate,
   }
 }
 
