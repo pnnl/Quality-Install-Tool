7dc10310b52e9fa652fbe09df9929e8cd9cb0910
holl695 <charlie.holly@pnnl.com>
2023-07-09 13:41:26 -0700

    Always get geo from GPS

--- src/utilities/photo_utils.tsx
+++ src/utilities/photo_utils.tsx
@@ -1,10 +1,6 @@
 import EXIF from 'exif-js'
 
 import Attachment from '../types/attachment.type'
-import { parse, format } from 'date-format-parse'
-import {degMinSecToDeg} from './deg_mins_sec_to_dec'
-import JSONValue from '../types/json_value.type';
-import Metadata from '../types/metadata.type';
 
 
 /**
@@ -19,7 +15,7 @@ function getCurrentGeolocation(): Promise<GeolocationPosition> {
       reject, 
       {
         // Allow a cached GPS value to be used for up to a minute
-        maximumAge: 1 * 60 * 1000,
+        maximumAge: 0,
         // Assume the GPS is unavailable after a minute
         timeout: 60 * 1000
       }
@@ -42,7 +38,7 @@ function getCurrentGeolocation(): Promise<GeolocationPosition> {
  *  timestamp
  * }
  */
-async function getMetadataFromCurrentGPSLocation() : Promise<Attachment["metadata"]> {
+export async function getMetadataFromCurrentGPSLocation() : Promise<Attachment["metadata"]> {
   const position = await getCurrentGeolocation()
   const metadata = {
     geolocation: {
@@ -50,101 +46,13 @@ async function getMetadataFromCurrentGPSLocation() : Promise<Attachment["metadat
       latitude: position.coords.latitude || null,
       longitude: position.coords.longitude || null,
     },
-    timestamp:  format(new Date(position.timestamp),'YYYY:MM:DD HH:mm:ss') || null
+    // Do NOT get the timestamp from position because getCurrentGeolocation
+    // may return a cached GeolocationPosition if the lat, long have not 
+    // changed sufficiently.
+    timestamp:  new Date(Date.now()).toUTCString()
   }
-
   return metadata
 }
 
-/**
- * Retrieves geolocation data from the Photo
- * @param photo 
- * @returns An object of the form:
- * {
- *  geolocation: {
- *    altitude,
- *    latitude,
- *    longitude
- *  },
- *  timestamp
- * }
- */
-function getMetadataFromPhoto(photo: Blob) : Promise<Attachment["metadata"]>  {
-  
-  return new Promise((resolve) => {
-    EXIF.getData(photo, function() {
-      const fullMetaData = EXIF.getAllTags(this)
-      const {DateTimeOriginal, GPSAltitude, GPSLatitude, GPSLatitudeRef, GPSLongitude, GPSLongitudeRef} = fullMetaData
-      const metadata = {
-        geolocation: {
-          altitude: GPSAltitude || null,
-          latitude: GPSLatitude && GPSLatitudeRef ? degMinSecToDeg(GPSLatitude, GPSLatitudeRef) : null,
-          longitude: GPSLongitude && GPSLongitudeRef ? degMinSecToDeg(GPSLongitude, GPSLongitudeRef) : null,
-        },
-        timestamp: DateTimeOriginal || null
-      }
-      resolve(metadata)
-    })
-  });
-}
-
-/**
- * Extracts the timestamp and geolocation data from a JPEG photo's
- * metadata
- * 
- * PhotoMetadata is retrieved from two different options 
- *        1. From Photo EXIF data   - getMetadataFromPhoto(photo)   
- *        2. From the devise GPS (if not present in Photo)    - getMetadataFromCurrentGPSLocation()  
- * 
- * @remarks
- * The caller must ensure that the photo is in jpeg format.
- * 
- * @param photo A Blob for a jpeg photo
- * @returns An object of the form:x`
- * {
- *  geolocation: {
- *    altitude,
- *    latitude,
- *    longitude
- *  },
- *  timestamp
- * }
- * 
- */
-export async function getPhotoMetadata(photo: Blob): Promise<Attachment["metadata"]> {
-  
-  // Fetching geolocation from photo
-  const metadataFromPhoto =  await getMetadataFromPhoto(photo)
- console.log('metadataFromPhoto', metadataFromPhoto)
-  let photoMetadata = metadataFromPhoto;
-  const {timestamp: photoTimestamp, geolocation: photoGelolocation} = metadataFromPhoto
- 
-  if (!photoTimestamp && !(photoGelolocation.latitude || photoGelolocation.longitude))  
-  {
-    //This case routinely happens when taking a photo using an iOS camera
-    photoMetadata = await getMetadataFromCurrentGPSLocation()
-    console.log('metadataFromGPS', photoMetadata)
-  }
-  else if (photoTimestamp)  //Metadata from Photo do not have required geotagging data.
-  {
-
-      const metadatafromGPS = await getMetadataFromCurrentGPSLocation()
-      // Calculating the date difference between Timestamp from Photo and current location GPS data
-      const datefromPhoto = new Date(parse(metadataFromPhoto.timestamp,'YYYY:MM:DD HH:mm:ss'))
-      const datefromGPS = new Date(parse(metadatafromGPS.timestamp,'YYYY:MM:DD HH:mm:ss'))
-      var diff = (datefromGPS.getTime() -  datefromPhoto.getTime()) / 1000;
-      var hr_diff = diff/ (60 * 60 *24)
-      // if time from Photo is than 2 hrs from the current GPS time, updating the GPS location data from device
-      if (hr_diff < 25 )
-      {
-        photoMetadata = {timestamp: photoTimestamp, geolocation: metadatafromGPS.geolocation}
-        console.log('metadataFromBoth:',photoMetadata )
-      }
-  }
-   console.log('photoMetadata:', photoMetadata)
-  // return photoMetadata  
-  return photoMetadata
-    
-  }
   
 
