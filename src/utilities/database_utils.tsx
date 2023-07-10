1e0e970b53bd46632d6e8882c3664b5ec3c97ca8
Rae Chen <cxy7562@gmail.com>
2023-07-10 12:31:12 -0700

    add doc

--- src/utilities/database_utils.tsx
+++ src/utilities/database_utils.tsx
@@ -1,3 +1,11 @@
+/**
+ * Adds a new document to the PouchDB database with the provided name and date.
+ * @param {PouchDB.Database<{}>} db - The PouchDB database instance.
+ * @param {string} name - The name of the document to be added.
+ * @param {Date} date - The date of creation for the document.
+ * @returns {Promise<void>} - A Promise that resolves when the document is successfully added.
+ */
+
 export async function putNewDoc(db : PouchDB.Database<{}>, name:string, date:Date) {
     db.putIfNotExists(name, {metadata_:{created_at: date, last_modified_at: date, attachments: {}}})
 }
