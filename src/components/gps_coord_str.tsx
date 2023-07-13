5e2802faf61595214397dcf34f59b130f3d2ea0c
Michael Georgescu <michael.georgescu@pnnl.gov>
2023-07-13 12:44:05 -0700

    whitespace

--- src/components/gps_coord_str.tsx
+++ src/components/gps_coord_str.tsx
@@ -4,16 +4,16 @@ import PhotoMetadata from '../types/photo_metadata.type'
 import { loadPartialConfig } from '@babel/core'
 
 /**
- * 
- * @param dec Geographic degrees 
- * @returns 
+ *
+ * @param dec Geographic degrees
+ * @returns
  */
 const GpsCoordStr: FC<PhotoMetadata["geolocation"]> = (geolocation) => {
   const {latitude, longitude} = geolocation
   const geoStr = latitude && longitude ? Number(latitude).toFixed(4) + "," + Number(longitude).toFixed(4) : null
-  
+
   return (
-    <> 
+    <>
       {geoStr ? <a href={"https://www.google.com/maps/search/?api=1&query=" + geoStr} target="_blank">{geoStr}</a> : <span>Missing</span>}
     </>
   )
