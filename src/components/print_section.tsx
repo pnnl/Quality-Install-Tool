2ccde3df51e11a1760007adfdd9683828908d123
Michael Georgescu <michael.georgescu@pnnl.gov>
2023-07-14 08:05:15 -0700

    added st address suffix to document / filename

--- src/components/print_section.tsx
+++ src/components/print_section.tsx
@@ -8,6 +8,7 @@ import ReactToPrint, { useReactToPrint } from 'react-to-print'
 interface PrintSectionProps {
   children: ReactNode,
   label: string
+  filename_suffix: string
 }
 
 /**
@@ -16,18 +17,22 @@ interface PrintSectionProps {
  * @param children Content for printing 
  * @param label Label for the print button
  */
-const PrintSection: FC<PrintSectionProps> = ({children, label}) => {
+const PrintSection: FC<PrintSectionProps> = ({children, label, filename_suffix}) => {
   const printContainerId = useId();
   return (
     <>
-      <Button onClick={event => print({
+      <Button onClick={event => {
+        let title = 'BASC QA Tool'
+        if (filename_suffix) {title += ' - ' + filename_suffix}
+        document.title = title
+        print({
           maxWidth: 2000,
           printable: printContainerId,
           type: 'html', 
           targetStyles: ["*"],
           css: '/print.css',
           documentTitle: 'DOE - Quality Installation Report',
-        })} 
+        })}} 
         variant="primary">
         {label}
       </Button>
