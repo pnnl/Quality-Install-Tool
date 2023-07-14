ee110299a3d724e67f04c99b1acf2d71dad2cccc
Michael Georgescu <michael.georgescu@pnnl.gov>
2023-07-13 21:40:30 -0700

    continuing fixing linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,21 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    // 'standard-with-typescript'
+    'standard-with-typescript'
     // 'prettier'
   ],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+    project: './tsconfig.json',
     sourceType: 'module'
   },
   plugins: [
     'react'
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": 0 // This should be removed. Need to figure out a way to define proptypes on string_input_modal
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0 // This should be removed. Need to figure out a way to define proptypes on string_input_modal
   }
 }
