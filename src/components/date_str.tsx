db8667bc54d84ee1603bc3125b6d2d84aebb5224
Sudha Eswaran <sudhacheran@gmail.com>
2023-07-28 10:31:30 -0700

    DateStr Change after Review

--- src/components/date_str.tsx
+++ src/components/date_str.tsx
@@ -20,28 +20,16 @@ const DEFAULT_OPTIONS: Intl.DateTimeFormatOptions = {
 };
 
 /**
- * getDate function: Returns Date() object from the date string of format ('YYYY-MM-DD')
- * @param dateStr : Input from the template
- * @returns : Date() 
- */
-function getDate(dateStr: string): Date {
-  if (isEmpty(dateStr)) return new Date(dateStr)
-  const dateArray = dateStr.split("-")
-  const _entryDate = new Date(parseInt(dateArray[0]), parseInt(dateArray[1], 10) - 1, parseInt(dateArray[2]))
-  return _entryDate
-}
-
-/**
- * DateStr component
+ * DateStr component : Returns date only in UTC format
  * @param date - A string appropriate as the single argument to the Date() constructor
  * @param locals - An optional string used as the first argument to the Date.toLocalDateString() method (default: 'en-us')
  * @param options - An optional objected used as the second argument to the Date.toLocalDateString() method
  * @returns The formatted date component
  */
 const DateStr: FC<DateStrProps> = ({ date, locals = 'en-us', options = {} }) => {
-  const full_options = { ...DEFAULT_OPTIONS, ...options }
-  const parsedDate = getDate(date)
-  const formattedDate = parsedDate.toLocaleString(locals, full_options)
+  const full_options = { ...DEFAULT_OPTIONS, ...options , timeZone:'UTC'}
+  const parsedDate = new Date(date)
+  const formattedDate = parsedDate.toLocaleDateString(locals, full_options)
   return <span>{formattedDate}</span>
 }
 
