3d65687c5d2bd8b8ea867956dbb392e27fa5d22c
Rae Chen <cxy7562@gmail.com>
2023-08-07 15:44:19 -0700

    adding regex, delete separator

--- src/templates/templates_config.ts
+++ src/templates/templates_config.ts
@@ -4,24 +4,28 @@ import OldQaHPWHTemplate from './old-qa_hpwh.mdx'
 import MdxPlayground from './playground.mdx'
 import { MDXProps } from 'mdx/types';
 
-interface TemplateConfig {
+interface TemplatesConfig {
   [key: string]: {
     title: string;
     template: (props: MDXProps) => JSX.Element;
   };
 }
 
-const templateRegex = /^[a-zA-Z0-9!@#$%^&*()-+=_{}|:;"'<>,.?~`]{1,64}$/ //TODO: decide on the Regex
+const templateRegex = /^(?!_)(?!.*_$)[a-z0-9_]{1,64}$/
 
-const templatesConfig : TemplateConfig = {
+const templatesConfig : TemplatesConfig = {
   doe_workflow_hpwh: {
     title: "Heat Pump Water Heater",
     template: DOEWorkflowHPWHTemplate,
   }
   // Add other template configurations here...
 }
-
-const validateTemplatesConfig = (config: TemplateConfig) => {
+/**
+ * Validates a TemplatesConfig object by checking if template names adhere to templateRegex pattern.
+ * @param {TemplatesConfig} config - The TemplatesConfig object to validate.
+ * @throws {Error} Throws an error if one or more template names are not allowed.
+ */
+function validateTemplatesConfig(config: TemplatesConfig) {
   const isValidKeys = Object.keys(config).every((key) =>
     {
       if(!templateRegex.test(key)){
@@ -30,12 +34,9 @@ const validateTemplatesConfig = (config: TemplateConfig) => {
       return templateRegex.test(key)
     }
   );
-
-  if (isValidKeys) {
-    return config;
-  } else {
     throw new Error("one or more template names are not allowed") //Decide on what to do if not pass
-  }
 };
 
-export default validateTemplatesConfig(templatesConfig);
+validateTemplatesConfig(templatesConfig);
+
+export default templatesConfig;
--- src/utilities/paths_utils.tsx
+++ src/utilities/paths_utils.tsx
@@ -1,8 +1,7 @@
 import { isUndefined, toPath } from 'lodash'
 
-const pathRegex = /^.{1,64}$/ //TODO: decide on what regex to use.
-const prefixRegex = /^.{1,64}$/ //TODO: decide on what regex to use.
-const separatorRegex = /^.{1,64}$/ //TODO: decide on what regex to use.
+const pathRegex = /^(?![-])(?!.*[-]$)[a-z0-9-]{1,64}$/
+const prefixRegex = /^[a-z0-9]{1,64}$/
 
 /**
  * Utility function used to convert a string path into a string 
@@ -11,24 +10,21 @@ const separatorRegex = /^.{1,64}$/ //TODO: decide on what regex to use.
  * @param path A string representing a path into a JSON-like object
  * (e.g. ""location[1].state"")
  * @param prefix (OPTIONAL) A string used as a first path component (default: "path")
- * @param separator (OPTIONAL) String used to separate the path components (default: "-")
  * @returns 
  */
-export function pathToId(path: string, prefix?: string, separator?:string): string {
+export function pathToId(path: string, prefix?: string): string {
   if (!prefix){
     prefix = "path";
   }
-  if(!separator){
-    separator = "-"
-  }
-  if(!pathRegex.test(path)){
-    throw new Error("path name " + path +" is not allowed"); //TODO: decide what to do when not pass
+  const separator = "-"
+  
+  const wholePath = [prefix, ...toPath(path)].join(separator);
+  if(!pathRegex.test(wholePath)){
+    throw new Error("result path name " + wholePath +" is not allowed"); //TODO: decide what to do when not pass
   } else if(!prefixRegex.test(prefix)){
     throw new Error("prefix name " + prefix +" is not allowed"); //TODO: decide what to do when not pass
-  } else if(!separatorRegex.test(separator)){
-    throw new Error("separator char " + separator + " is not allowed"); //TODO: decide what to do when not pass
   }
   else {
-    return [prefix, ...toPath(path)].join(separator)
+    return wholePath
   }
 }
