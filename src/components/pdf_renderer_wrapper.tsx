595dc621cd756c3be4c2d06ea335ef3503684e13
Sudha Eswaran <sudhacheran@gmail.com>
2023-08-24 11:29:30 -0700

    code comments text changed

--- src/components/pdf_renderer_wrapper.tsx
+++ src/components/pdf_renderer_wrapper.tsx
@@ -11,11 +11,11 @@ interface PDFRenderedWrapperProps {
 }
 
 /**
- * A component that wraps a FileRenderer component in order to tie it to the data store
+ * A component that wraps a PDFRenderer component in order to tie it to the data store
  *
  * @param children Content (most commonly markdown text) to be passed on as the children
  * @param id An identifier for the store attachment that represents the File details
- * @param label The label the FileRenderer component
+ * @param label The label of the PDFRenderer component
  */
 const PDFRenderedWrapper: FC<PDFRenderedWrapperProps> = ({
     children,
