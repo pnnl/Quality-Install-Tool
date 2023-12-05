99ed867d22cfe8d8b6fcdb8e689bbf24608e4851
Sudha Eswaran <sudha.eswaran@pnnl.gov>
2023-12-05 09:43:31 -0800

    removed unused import

--- src/components/string_input_modal.tsx
+++ src/components/string_input_modal.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState, useEffect } from 'react'
+import { SetStateAction, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import Modal from 'react-bootstrap/Modal'
 import PropTypes from 'prop-types'
