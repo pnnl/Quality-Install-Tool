46b81f6200de6a0a82c6c1ff32f4ff3392398bd0
Sudha Eswaran <sudha.eswaran@pnnl.gov>
2024-07-11 16:04:03 -0700

    IRA workflows

--- src/templates/templates_config.ts
+++ src/templates/templates_config.ts
@@ -6,6 +6,10 @@ import DOEWorkflowCentralDuctedSplitHeatPumpTemplate from './doe_workflow_centra
 import IRADOEWorkflowHPWHTemplate from './ira_doe_workflow_hpwh.mdx'
 import IRADOEWorkflowDuctlessHeatPumpTemplate from './ira_doe_workflow_ductless_heat_pump.mdx'
 import IRADOEWorkflowCentralDuctedSplitHeatPumpTemplate from './ira_doe_workflow_central_ducted_split_heat_pump.mdx'
+import IRADOEWorkflowHighEfficiencyGasFurnace from './ira_doe_workflow_high_efficiency_gas_furnace.mdx'
+import IRADOEWorkflowHighEfficiencyTanklessWaterHeater  from './ira_doe_workflow_high_efficiency_tankless_water_heater.mdx'
+import IRADOEWorkflowHighEfficiencyModulatingBoiler  from './ira_doe_workflow_high_efficiency_modulating_boiler.mdx'
+
 
 import { MDXProps } from 'mdx/types'
 
@@ -56,6 +60,19 @@ const IRA_WORKFLOW_TEMPLATES = {
         title: 'IRA - Heat Pump Water Heater',
         template: IRADOEWorkflowHPWHTemplate,
     },
+    ira_doe_workflow_high_efficiency_gas_furnace: {
+        title: 'IRA - High Efficiency Gas Furnace',
+        template: IRADOEWorkflowHighEfficiencyGasFurnace,
+    },
+    ira_doe_workflow_high_efficiency_tankless_water_heater: {
+        title: 'IRA - High Efficiency Tankless Water Heater',
+        template: IRADOEWorkflowHighEfficiencyTanklessWaterHeater,
+    },
+    ira_doe_workflow_high_efficiency_modulating_boiler: {
+        title: 'IRA - High Efficiency Modulating Boiler',
+        template: IRADOEWorkflowHighEfficiencyModulatingBoiler,
+    },
+
 }
 
 /**
