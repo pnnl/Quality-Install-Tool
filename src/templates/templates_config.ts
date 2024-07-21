abde37ec62545546921c7b29bb1b83de2e529af2
holl695 <charlie.holly@pnnl.com>
2024-07-21 15:06:47 -0700

    Full frame window replacement template

--- src/templates/templates_config.ts
+++ src/templates/templates_config.ts
@@ -7,7 +7,7 @@ import IRADOEWorkflowHPWHTemplate from './ira_doe_workflow_hpwh.mdx'
 import IRADOEWorkflowDuctlessHeatPumpTemplate from './ira_doe_workflow_ductless_heat_pump.mdx'
 import IRADOEWorkflowCentralDuctedSplitHeatPumpTemplate from './ira_doe_workflow_central_ducted_split_heat_pump.mdx'
 import IRADOEWorkflowHighEfficiencyGasFurnace from './ira_doe_workflow_high_efficiency_gas_furnace.mdx'
-import IRADOEWorkflowHighEfficiencyTanklessWaterHeater from './ira_doe_workflow_high_efficiency_tankless_water_heater.mdx'
+import IRADOEWorkflowHighEfficiencyWaterHeater from './ira_doe_workflow_high_efficiency_water_heater.mdx'
 import IRADOEWorkflowHighEfficiencyModulatingBoiler from './ira_doe_workflow_high_efficiency_modulating_boiler.mdx'
 import IRADOEWorkflowFullFrameReplacementWindows from './ira_doe_workflow_full_frame_replacement_windows.mdx'
 import IRADOEWorkflowInsertReplacementWindows from './ira_doe_workflow_insert_replacement_windows.mdx'
@@ -65,9 +65,9 @@ const IRA_WORKFLOW_TEMPLATES = {
         title: 'IRA - High Efficiency Gas Furnace',
         template: IRADOEWorkflowHighEfficiencyGasFurnace,
     },
-    ira_doe_workflow_high_efficiency_tankless_water_heater: {
-        title: 'IRA - High Efficiency Tankless Water Heater',
-        template: IRADOEWorkflowHighEfficiencyTanklessWaterHeater,
+    ira_doe_workflow_high_efficiency_water_heater: {
+        title: 'IRA - High Efficiency Water Heater',
+        template: IRADOEWorkflowHighEfficiencyWaterHeater,
     },
     ira_doe_workflow_high_efficiency_modulating_boiler: {
         title: 'IRA - High Efficiency Modulating Boiler',
