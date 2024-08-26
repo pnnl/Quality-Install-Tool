3ae106c153c863df9b497821ae59c05c09219ca0
Sudha Eswaran <sudha.eswaran@pnnl.gov>
2024-08-26 12:37:51 -0700

    Merging all workflows into one app

--- src/templates/templates_config.ts
+++ src/templates/templates_config.ts
@@ -24,7 +24,7 @@ interface TemplatesConfig {
 const templateRegex = /^(?!_)(?!.*_$)[a-z0-9_]{1,64}$/
 
 // Add workflow templates for 'quality-install-tool'
-const GENERAL_WORKFLOW_TEMPLATES = {
+const templatesConfig: TemplatesConfig = {
     doe_workflow_attic_air_sealing: {
         title: 'Attic Air Sealing',
         template: DOEWorkflowAtticAirSealTemplate,
@@ -33,86 +33,39 @@ const GENERAL_WORKFLOW_TEMPLATES = {
         title: 'Attic Insulation',
         template: DOEWorkflowAtticInsulationTemplate,
     },
-    doe_workflow_central_ducted_split_heat_pump: {
-        title: 'Heat Pump Ducted',
-        template: DOEWorkflowCentralDuctedSplitHeatPumpTemplate,
+    ira_doe_workflow_full_frame_replacement_windows: {
+        title: 'Full Frame Replacement Windows',
+        template: IRADOEWorkflowFullFrameReplacementWindows,
     },
-    doe_workflow_ductless_heat_pump: {
-        title: 'Heat Pump Ductless',
-        template: DOEWorkflowDuctlessHeatPumpTemplate,
-    },
-    doe_workflow_hpwh: {
-        title: 'Heat Pump Water Heater',
-        template: DOEWorkflowHPWHTemplate,
-    },
-}
-
-// Add workflow templates for 'ira-quality-install-tool'
-const IRA_WORKFLOW_TEMPLATES = {
     ira_doe_workflow_central_ducted_split_heat_pump: {
-        title: 'IRA - Heat Pump Ducted',
+        title: 'Heat Pump Ducted',
         template: IRADOEWorkflowCentralDuctedSplitHeatPumpTemplate,
     },
     ira_doe_workflow_ductless_heat_pump: {
-        title: 'IRA - Heat Pump Ductless',
+        title: 'Heat Pump Ductless',
         template: IRADOEWorkflowDuctlessHeatPumpTemplate,
     },
     ira_doe_workflow_hpwh: {
-        title: 'IRA - Heat Pump Water Heater',
+        title: 'Heat Pump Water Heater',
         template: IRADOEWorkflowHPWHTemplate,
     },
     ira_doe_workflow_high_efficiency_gas_furnace: {
-        title: 'IRA - High Efficiency Gas Furnace',
+        title: 'High Efficiency Gas Furnace',
         template: IRADOEWorkflowHighEfficiencyGasFurnace,
     },
     ira_doe_workflow_high_efficiency_water_heater: {
-        title: 'IRA - High Efficiency Water Heater',
+        title: 'High Efficiency Water Heater',
         template: IRADOEWorkflowHighEfficiencyWaterHeater,
     },
     ira_doe_workflow_high_efficiency_modulating_boiler: {
-        title: 'IRA - High Efficiency Modulating Boiler',
+        title: 'High Efficiency Modulating Boiler',
         template: IRADOEWorkflowHighEfficiencyModulatingBoiler,
     },
-    ira_doe_workflow_full_frame_replacement_windows: {
-        title: 'IRA - Full Frame Replacement Windows',
-        template: IRADOEWorkflowFullFrameReplacementWindows,
-    },
     ira_doe_workflow_insert_replacement_windows: {
-        title: 'IRA - Insert Replacement Windows',
+        title: 'Insert Replacement Windows',
         template: IRADOEWorkflowInsertReplacementWindows,
     },
 }
-
-/**
- * Configure and render workflow templates based on the deployment environment configured in AWS S3 or local development.
- *
- * When deploying the application to AWS S3, the environment variable 'REACT_APP_ENV'
- * is set to 'quality-install-tool' or 'ira-quality-install-tool' for the respective applications.
- *
- * In local development, the 'REACT_APP_ENV' environment variable can be set directly in the 'package.json'
- * under the start script, as shown below:
- *
- * ```
- * "scripts": {
- *   "start": "HTTPS=true REACT_APP_ENV=ira-quality-install-tool node scripts/start.js",
- *   ...
- * }
- * ```
- *
- * The environment variable 'REACT_APP_ENV' is accessed in code using the `process.env` object,
- * which will be populated with the specified value ('quality-install-tool' or 'ira-quality-install-tool')
- * during the build process or local development startup.
- *
- * The `templatesConfig` constant determines which set of workflow templates (`GENERAL_WORKFLOW_TEMPLATES`
- * or `IRA_WORKFLOW_TEMPLATES`) to use based on the value of `process.env.REACT_APP_ENV`.
- *
- * The respective environments ('quality-install-tool' or 'ira-quality-install-tool').
- */
-
-const templatesConfig: TemplatesConfig =
-    process.env.REACT_APP_ENV === 'quality-install-tool'
-        ? GENERAL_WORKFLOW_TEMPLATES
-        : IRA_WORKFLOW_TEMPLATES
 
 // Assuming TemplatesConfig is defined somewhere as a type or interface
 
