694f742603f866a030e7e5896e091d3f9fc5d857
Mark Borkum <mark.borkum@pnnl.gov>
2024-09-19 12:01:17 -0700

    Remove "source:" text from GpsCoordStr and DateTimeStr components.

--- src/components/gps_coord_str.tsx
+++ src/components/gps_coord_str.tsx
@@ -38,11 +38,6 @@ const GpsCoordStr: FC<GpsCoordStrProps> = geolocation => {
                     >
                         {geoStr}
                     </a>
-                    {source ? (
-                        <span style={{ marginLeft: '2px' }}>
-                            (source: {source})
-                        </span>
-                    ) : null}
                 </>
             ) : (
                 <span>Missing</span>
