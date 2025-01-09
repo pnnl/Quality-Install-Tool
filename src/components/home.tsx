6d705890b0f8f2be46d39bdbd8a4df774d645148
Lindsay Johnston <lindsay.johnston@pnnl.gov>
2025-01-08 17:09:59 -0700

    249 : Undid a change I didn't mean to push.

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -126,7 +126,7 @@ const Home: FC = () => {
     }
 
     const editAddressDetails = (projectID: string) => {
-        navigate('app/' + projectID)
+        navigate('app/' + projectID, { replace: true })
     }
 
     const projects_display =
