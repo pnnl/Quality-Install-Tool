5c38d737541c7dea21582694f342b3716ebb50c9
Mark Borkum <mark.borkum@pnnl.gov>
2025-02-09 11:36:20 -0800

    Remove redundant comments

--- src/App.tsx
+++ src/App.tsx
@@ -17,31 +17,24 @@ import DatabaseProvider from './providers/database_provider'
 const router = createBrowserRouter([
     {
         path: '/',
-        // App Home page : Lists existing projects provides functionality to create new one
         element: <ProjectsListView />,
     },
     {
         path: `/app/:projectId`,
-        // Project details view: Collects information related to the project.
         element: <ProjectsEditView />,
     },
     {
         path: `/app/:projectId/workflows`,
-        // Workflow list view:  List the names of workflows available for generating installation report.
         element: <ProjectsShowView />,
     },
     {
         path: `/app/:projectId/:workflowName`,
-        // Jobs List View: Lists existing installations associated with a particular workflow
-        // and provides functionality to create new installations
         element: <InstallationsListView />,
     },
     {
         path: `/app/:projectId/:workflowName/:jobId`,
-        // Jobs View: Gathering and displaying information pertinent to individual installations
         element: <InstallationsEditView />,
     },
-    // TODO: This route will be revisited and revised in the future
     // {
     //     path: `/app/:projectId/:workflowName/:jobId/json`,
     //     element: (
@@ -50,7 +43,6 @@ const router = createBrowserRouter([
     //         </Layout>
     //     ),
     // },
-    // TODO: This route will be revisited and revised in the future
     // {
     //     path: '/template_editor',
     //     element: <TemplateEditor />,
