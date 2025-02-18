8cfc5b5d22cda3cda5594757043416c1fd4ef614
Mark Borkum <mark.borkum@pnnl.gov>
2025-02-18 08:25:48 -0800

    Remove `@typescript-eslint/no-explicit-any` rule override

--- src/utilities/path_utils.ts
+++ src/utilities/path_utils.ts
@@ -2,35 +2,48 @@ import { toPath } from 'lodash'
 
 type NonEmptyArray<T> = [T, ...Array<T>]
 
-function _immutableUpsert<T>(
+function _immutableUpsert<T extends Array<unknown> | Record<string, unknown>>(
     path: NonEmptyArray<string>,
     sender: T,
     value: unknown,
 ): T {
     const [propName, ...nextPath] = path
 
-    const receiver: any =
-        typeof sender === 'object'
-            ? Array.isArray(sender)
-                ? [...sender]
-                : { ...sender }
-            : isNaN(parseInt(propName))
-              ? {}
-              : []
+    if (Array.isArray(sender)) {
+        const receiver = [...sender]
 
-    receiver[propName] =
-        nextPath.length === 0
-            ? value
-            : _immutableUpsert(
-                  nextPath as NonEmptyArray<string>,
-                  receiver[propName],
-                  value,
-              )
+        const index = parseInt(propName)
 
-    return receiver
+        receiver[index] =
+            nextPath.length === 0
+                ? value
+                : _immutableUpsert(
+                      nextPath as NonEmptyArray<string>,
+                      sender[index] as Array<unknown> | Record<string, unknown>,
+                      value,
+                  )
+
+        return receiver as T
+    } else {
+        return {
+            ...sender,
+            [propName]:
+                nextPath.length === 0
+                    ? value
+                    : _immutableUpsert(
+                          nextPath as NonEmptyArray<string>,
+                          sender[propName] as
+                              | Array<unknown>
+                              | Record<string, unknown>,
+                          value,
+                      ),
+        }
+    }
 }
 
-export function immutableUpsert<T>(path: string, sender: T, value: unknown): T {
+export function immutableUpsert<
+    T extends Array<unknown> | Record<string, unknown>,
+>(path: string, sender: T, value: unknown): T {
     return _immutableUpsert<T>(
         toPath(path) as NonEmptyArray<string>,
         sender,
