234ca0ae37e93b80233f440c59fb28f58dbab57d
Sudha Eswaran <sudhacheran@gmail.com>
2025-03-05 15:33:32 -0800

    service worker chages for testing

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -35,10 +35,12 @@ export function register(config?: Config) {
             // serve assets; see https://github.com/facebook/create-react-app/issues/2374
             return
         }
-
+        if (document.readyState === 'complete') {
+            const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+            registerValidSW(swUrl, config)
+        }
         window.addEventListener('load', () => {
             const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
                 checkValidServiceWorker(swUrl, config)
