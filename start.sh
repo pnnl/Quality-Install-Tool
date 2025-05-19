a38510409dde4d1e2d2cd7adba6f2b58a699ddad
Emily Armstrong <emilythearmstrong@gmail.com>
2025-05-19 16:12:24 -0400

    updates to add loadRuntimeConfig back in, proxy requests to express to 3001 via nginx.conf, update start.sh to forward logs from backend

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Start the backend
-node /app/server.js &
+# Start the backend and forward its logs to stdout/stderr
+node /app/server.js 2>&1 | sed 's/^/[EXPRESS] /' > /proc/1/fd/1 &
 
-# Start NGINX
+# Start NGINX (required so ECS doesn't think the container exited)
 nginx -g 'daemon off;'
