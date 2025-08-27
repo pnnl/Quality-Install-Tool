554c2d848c4458e4c45f8199276fd883fb14acb7
sudhacheran <sudhacherane@gmail.com>
2025-08-27 08:11:57 -0700

    DAP tag updated based on env

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,26 @@ import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
+const env = process.env.REACT_APP_ENV
+
+// Digital Analytics Program - Universal Analytics https://digital.gov/guides/dap/
+
+const analyticsSrcMap: Record<string, string> = {
+    production:
+        'https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=AGENCY&subagency=PNNL',
+    default:
+        'https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=HHS&subagency=PNNL&dapdev=true',
+}
+const analyticsSrc = analyticsSrcMap[env ?? ''] || analyticsSrcMap.default
+if (!document.getElementById('_fed_an_ua_tag')) {
+    const script = document.createElement('script')
+    script.id = '_fed_an_ua_tag'
+    script.async = true
+    script.type = 'text/javascript'
+    script.src = analyticsSrc
+    document.head.appendChild(script)
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
