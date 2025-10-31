afe5643fa6a17cc4568857ecc95636e1a3f55f1d
Sudha Eswaran <sudha.eswaran@pnnl.gov>
2025-10-31 10:27:38 -0700

    disabling the export icon until the download completes.

--- src/components/views/projects/list/export_document.tsx
+++ src/components/views/projects/list/export_document.tsx
@@ -1,5 +1,5 @@
 import PouchDB from 'pouchdb'
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { TfiImport } from 'react-icons/tfi'
 
@@ -26,28 +26,31 @@ const ExportDoc: React.FC<ExportDocProps> = ({
     showAlert,
 }) => {
     const db = useDatabase()
+    const [isDownloading, setIsDownloading] = useState(false)
 
     const handleClick = useCallback(
         async (event: React.MouseEvent<HTMLButtonElement>) => {
             event.stopPropagation()
             event.preventDefault()
+            setIsDownloading(true)
+            try {
+                const projectDoc = await getProject(db, projectId)
 
-            const projectDoc = await getProject(db, projectId)
+                const data = await exportJSONDocument(db, projectId)
 
-            const data = await exportJSONDocument(db, projectId)
+                const blob = new Blob([JSON.stringify(data)], {
+                    type: JSON_DOCUMENT_CONTENT_TYPE,
+                })
 
-            const blob = new Blob([JSON.stringify(data)], {
-                type: JSON_DOCUMENT_CONTENT_TYPE,
-            })
+                const fileName = `${projectDoc?.metadata_?.doc_name ?? 'project'
+                    } ${new Date().toUTCString()}${JSON_DOCUMENT_FILE_EXTENSION}`
 
-            const fileName = `${
-                projectDoc?.metadata_?.doc_name ?? 'project'
-            } ${new Date().toUTCString()}${JSON_DOCUMENT_FILE_EXTENSION}`
+                sendBlob(blob, fileName)
 
-            sendBlob(blob, fileName)
-
-            onDownload && (await onDownload())
-
+                onDownload && (await onDownload())
+            } finally {
+                setIsDownloading(false)
+            }
             return false
         },
         [db, projectId, onDownload],
@@ -57,6 +60,7 @@ const ExportDoc: React.FC<ExportDocProps> = ({
         <Button
             variant={variant}
             onClick={handleClick}
+            disabled={isDownloading}
             className={variant === 'outline-light' ? 'download-button' : ''}
             style={{ position: 'relative' }}
         >
