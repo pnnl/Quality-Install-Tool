af94dc9c2b43a689a1344b75630ae1f32bc5ffb3
Sudha Eswaran <sudha.eswaran@pnnl.gov>
2026-01-06 15:13:31 -0800

    workflow updates

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -23,7 +23,8 @@ import DOEWorkflowMechanicalVentilation from './ira_doe_workflow_mechanical_vent
 import DOEWorkflowSlapFoundationExterior from './ira_doe_workflow_slap_foundation_exterior_sealing_and_insulation.mdx'
 import DOEWorkflowWallAirSealingAndInsulation from './ira_doe_workflow_wall_air_sealing_and_insulation_dry_fill.mdx'
 import IRADOEWorkflowLimitedAssessment from './ira_doe_workflow_limited_assessment.mdx'
-import DOEAppliancesWorkflow from './doe_workflow_appliances.mdx'
+import DOEAppliancesWorkflow from './ira_doe_workflow_appliances.mdx'
+import DOEWorkflowWoodAndPelletStove from './ira_doe_workflow_wood_and_pallet_stove.mdx'
 // import Playground from './playground.mdx'
 
 import {
@@ -182,6 +183,11 @@ const TEMPLATES: Record<string, TemplateConfiguration> = {
             pluralLowerCase: 'assessments',
         },
         template: IRADOEWorkflowLimitedAssessment,
+    },
+    ira_doe_workflow_wood_and_pellet_stove: {
+        title: 'Wood and Pellet Stove Replacement',
+        subtitle: INSTALLATION_SUB_TITLE,
+        template: DOEWorkflowWoodAndPelletStove,
     },
     // playground: {
     //     title: 'Playground',
