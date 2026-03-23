6c73cf140323b7db2f24cd267d214676b3cf496f
sudhacde <sudha.eswaran@pnnl.gov>
2026-03-23 09:44:24 -0700

    included workflow search

--- src/components/views/projects/show/templates_list_group.tsx
+++ src/components/views/projects/show/templates_list_group.tsx
@@ -1,5 +1,5 @@
 import PouchDB from 'pouchdb'
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { ListGroup } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
@@ -20,46 +20,68 @@ const TemplatesListGroup: React.FC<TemplatesListGroupProps> = ({
     >(() => {
         return installations.reduce((accumulator, installation) => {
             const workflowName = installation.metadata_.template_name
-
             if (!accumulator.has(workflowName)) {
                 accumulator.set(workflowName, [])
             }
-
             accumulator.get(workflowName).push(installation)
-
             return accumulator
         }, new Map())
     }, [installations])
 
+    const [searchTerm, setSearchTerm] = useState('')
     const workflowNames = Object.keys(templates)
+    const filteredWorkflowNames = searchTerm
+        ? workflowNames.filter(
+              name =>
+                  templates[name].title
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase()) ||
+                  name.toLowerCase().includes(searchTerm.toLowerCase()),
+          )
+        : workflowNames
 
     if (workflowNames.length === 0) {
         return null
     } else {
         return (
-            <ListGroup>
-                {workflowNames.map(workflowName => {
-                    const workflowTitle = templates[workflowName].title
-
-                    const installationDocsCount =
-                        installationsByWorkflowName.get(workflowName)?.length ??
-                        0
-
-                    return (
-                        <LinkContainer
-                            key={workflowName}
-                            to={`/app/${projectId}/${workflowName}`}
-                        >
-                            <ListGroup.Item action={true}>
-                                {workflowTitle}
-                                {installationDocsCount > 0 && (
-                                    <> ({installationDocsCount})</>
-                                )}
-                            </ListGroup.Item>
-                        </LinkContainer>
-                    )
-                })}
-            </ListGroup>
+            <>
+                <div style={{ marginBottom: 16 }}>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search workflows by name..."
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                    />
+                </div>
+                <ListGroup>
+                    {filteredWorkflowNames.length > 0 ? (
+                        filteredWorkflowNames.map(workflowName => {
+                            const workflowTitle = templates[workflowName].title
+                            const installationDocsCount =
+                                installationsByWorkflowName.get(workflowName)
+                                    ?.length ?? 0
+                            return (
+                                <LinkContainer
+                                    key={workflowName}
+                                    to={`/app/${projectId}/${workflowName}`}
+                                >
+                                    <ListGroup.Item action={true}>
+                                        {workflowTitle}
+                                        {installationDocsCount > 0 && (
+                                            <> ({installationDocsCount})</>
+                                        )}
+                                    </ListGroup.Item>
+                                </LinkContainer>
+                            )
+                        })
+                    ) : (
+                        <ListGroup.Item>
+                            No matching workflows found.
+                        </ListGroup.Item>
+                    )}
+                </ListGroup>
+            </>
         )
     }
 }
